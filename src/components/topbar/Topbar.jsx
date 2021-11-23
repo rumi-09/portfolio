ecdc6928d8dbdcbedc851913fcee3448ce874d6a
rumi-09 <46239694+rumi-09@users.noreply.github.com>
2021-11-22 19:45:12 -0600

    left topbar done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import "./topbar.scss"
+import {Person, Mail} from '@material-ui/icons';
 
 export default function Topbar() {
     return (
-        <div className="topbar">
-            
+        <div className="topbar" id = "topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+44 924 12 74</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>masumarumi09@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    
+                </div>
+            </div>
         </div>
     )
 }
