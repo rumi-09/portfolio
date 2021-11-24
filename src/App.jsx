b863310ba7f9505ee206d039705f7a10693b6e2e
rumi-09 <46239694+rumi-09@users.noreply.github.com>
2021-11-24 14:32:38 -0600

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import Topbar from "./components/topbar/Topbar"
+import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro"
 import Portfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/Works"
 import Testimonials from "./components/testimonials/Testimonials"
-import Work from "./components/work/Work"
 import Contact from "./components/contact/Contact"
 import "./app.scss"
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar/>
-      <div className="sections">
-        <Intro />
-        <Portfolio />
-        <Work />
-        <Testimonials />
-        <Contact />
-      </div>
+     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     <div className="sections">
+       <Intro/>
+       <Portfolio/>
+       <Works/>
+       <Testimonials/>
+       <Contact/>
+     </div>
     </div>
   );
 }
